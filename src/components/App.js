2ac009c937c9ef3e9dfc4c457d92da3fd29756ff
Marta Castrillo <mart.castrillo@gmail.com>
2022-12-12 14:14:08 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import {Routes, Route, Link, NavLink} from 'react-router-dom';
+import '../styles/App.scss';
+
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
